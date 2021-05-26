a62ef68aec1b298b275cf465e29ec79347d5d53a
tunahanpnr <b21727652@cs.hacettepe.edu.tr>
2021-05-26 23:56:57 +0300

    joined club issue fixed

---
+++ klubber/src/main/java/com/spaghettiCoders/klubber/application/repository/AnsweredClubRepository.java
@@ -0,0 +1,10 @@
+package com.spaghettiCoders.klubber.application.repository;
+
+import com.spaghettiCoders.klubber.application.entity.AnsweredClub;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AnsweredClubRepository extends JpaRepository<AnsweredClub, Long> {
+    @Query("select case when (count(a) > 0)  then true else false end FROM AnsweredClub a WHERE a.clubName = :clubName and a.username = :username")
+    Boolean isAnswered(String username, String clubName);
+}
